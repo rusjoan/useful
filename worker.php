f5dcdf30bfbfcdd711a476bcf7c3191fa1d0d914
Evgeny Murashkin <rusjoan@ya.ru>
2017-11-16 23:15:24 +0300

    PHP worker signal processing example

---
+++ worker.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Author: Evgeny Murashkin
+ * Date: 16.11.2017
+ * Time: 22:20
+ */
+
+$exitFlag = false;
+$sec = 5;
+
+function e(string $text) {
+    print(date("H:i:s") . ": " . $text . "\n");
+}
+
+pcntl_signal(SIGTERM, function () {
+    global $exitFlag;
+    e("SIGTERM, setting exitFlag to TRUE");
+    $exitFlag = true;
+});
+
+while (true) {
+    e("checking signals...");
+    pcntl_signal_dispatch();
+    if ($exitFlag) {
+        e("SIGTERM found, exiting...");
+        break;
+    } else {
+        e("start sleep till " . date("H:i:s", time() + $sec - 1));
+        time_sleep_until(time() + $sec);
+        e("slept for $sec seconds, now checking exitFlag");
+    }
+}
